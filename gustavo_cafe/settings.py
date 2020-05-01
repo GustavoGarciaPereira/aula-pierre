3a1b90e768717ace27e30a8350abbd0c2c26c627
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-05-01 15:36:47 -0300

    deploy

--- gustavo_cafe/settings.py
+++ gustavo_cafe/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j^zd0)5jjy5+cogq1_vr#((8vi1&r%&kc7n0t29g(xy%0l7n!j'
-
+#SECRET_KEY = 'j^zd0)5jjy5+cogq1_vr#((8vi1&r%&kc7n0t29g(xy%0l7n!j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
