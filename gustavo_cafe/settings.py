97e77ef8de575740ac7d9fc7bf8299970038bde8
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-05-01 23:18:17 -0300

    deploy

--- gustavo_cafe/settings.py
+++ gustavo_cafe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aula-pierre.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['potifolio-cafe.herokuapp.com','127.0.0.1']
 
 
 # Application definition
